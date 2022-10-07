208465390475dd672eec264c2952c671a41ee6ad
henry <htat@cpp.edu>
2022-10-07 16:34:26 -0700

    Created insert marker and beginning of game option

--- tictactoe.c
+++ tictactoe.c
@@ -2,11 +2,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("WELCOME TO TIC TAC TOE\n");
+int gameOption;
+char turn = 'X';
+char board[] = { '-','-','-','-','-','-','-','-','-' }; 
+printf("WELCOME TO TIC TAC TOE\n");
+createBoard(board); 
 
+}
 /*Create Board */ 
-char board[] = { '-','-','-','-','-','-','-','-','-' };                              
-   int i;
+void createBoard(char board[]){
+    int i;
    for (i = 0; i < 9; i++) {
       printf("%c ", board[i]);
       if (((i + 1) % 3) == 0) {
@@ -14,9 +19,21 @@ char board[] = { '-','-','-','-','-','-','-','-','-' };
       }
    }
 }
-
-
 /*prompt user for game they wish to play (PVP or PVE) */
+void gameOption(int gameOption){
+    printf("Select an Option:\n");
+    printf("1 -- person vs. person\n2 -- person vs. random computer\n");
+    scanf("%d",&gameOption);
+    if(gameOption==1)
+
+}
+
+void insertMarker(char board[], char *turn){
+    int cords;
+    printf("Player %c turn\nEnter cords: ", *turn);
+    scanf("%d",&cords);
+    board[cords-1] = *turn;
+}
 
 /*Present option for player 1 */
 
