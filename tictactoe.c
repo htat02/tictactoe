5ff73042eb3fc7b9e805ed307b0075d257041362
henry <htat@cpp.edu>
2022-10-07 15:47:22 -0700

    Create board

--- tictactoe.c
+++ tictactoe.c
@@ -1,12 +1,22 @@
 /* Tic Tac Toe game with person vs person or person vs cpu */
+#include <stdio.h>
 
-/*prompt user for game they wish to play (PVP or PVE) */
-
-/*Present option for player 1 */
-
+int main(){
+    printf("WELCOME TO TIC TAC TOE\n");
 
 /*Create Board */ 
+char board[] = { '-','-','-','-','-','-','-','-','-' };                              
+   int i;
+   for (i = 0; i < 9; i++) {
+      printf("%c ", board[i]);
+      if (((i + 1) % 3) == 0) {
+         printf("\n");
+      }
+   }
+}
 
+
+/*prompt user for game they wish to play (PVP or PVE) */
 
 /*Present option for player 1 */
 
