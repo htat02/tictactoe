f48eed60b2448fcfab915c0b530b64d0ccddc55d
henry <htat@cpp.edu>
2022-10-07 15:28:54 -0700

    First Outline

---
+++ tictactoe.c
@@ -0,0 +1,15 @@
+/* Tic Tac Toe game with person vs person or person vs cpu */
+
+/*prompt user for game they wish to play (PVP or PVE) */
+
+/*Present option for player 1 */
+
+
+/*Create Board */ 
+
+
+/*Present option for player 1 */
+
+/*Present option for player 2 if available */ 
+
+/*CPU action */
